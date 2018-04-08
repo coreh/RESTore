5cda81471f639130accaaf396fc410116e1a802f
Marco Aurelio <thecoreh@gmail.com>
2018-04-08 02:20:25 -0300

    Move test to example

---
+++ example.ts
@@ -0,0 +1,42 @@
+import RESTore from '.';
+
+const store = new RESTore();
+
+store.use('/users/:id', async function (params, options, path, next) {
+    switch (options.method) {
+        case 'GET':
+            return this.stored(path);
+        case 'PATCH':
+            const stored = this.stored(path);
+            return {
+                ...stored,
+                ...options.body,
+            };
+        default:
+            return next();
+    }
+});
+
+store.use('/users', async function (params, options, path, next) {
+    switch (options.method) {
+        case 'POST':
+            return {
+                [RESTore.Path]: `/users/${options.body.username}`,
+                ...options.body,
+            };
+        default:
+            return next();
+    }
+});
+
+async function main() {
+    await store.post('/users', {
+        username: 'coreh',
+        likes: ['Chocolate', 'Coffee'],
+    });
+
+    const user = await store.get('/users/coreh');
+    console.log(user); // { username: 'coreh', likes: ['Chocolate', 'Coffee'] }
+}
+
+main();
