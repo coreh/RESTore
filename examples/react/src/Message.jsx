2b743b7636176d682f64f362c31c2b5ac9721094
Marco Aurelio <thecoreh@gmail.com>
2018-04-22 01:42:42 -0300

    Add note about .take() throwing

--- examples/react/src/Message.jsx
+++ examples/react/src/Message.jsx
@@ -5,6 +5,9 @@ import StoreContext from './StoreContext';
 const Message = (props) => (
     <StoreContext.Consumer>
         {store => {
+            // Calling .take() instead of .get() here throws a Promise,
+            // causing the React rendering to suspend and resume at a
+            // later time when the resource becomes available
             const message = store.take(props.path);
             return <li className={message.state}>{message.message}</li>;
         }}
